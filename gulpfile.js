6d178fbaf974492b5a6573bed67b417926a9af41
Anastasiya <nastya.mazurova.2016@gmail.com>
2018-09-30 16:29:40 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var scss = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var cleanCss = require('gulp-clean-css');
+var gulpIf = require('gulp-if');
+var browserSync = require('browser-sync').create();
+
+var config = {
+    paths: {
+        scss: './src/scss/**/*.scss',
+        html: './public/index.html'
+    },
+    output: {
+        cssName: 'bundle.min.css',
+        path: './public'
+    },
+    isDev: true
+};
+
+gulp.task('scss', function(){
+    return gulp.src(config.paths.scss)
+        .pipe(gulpIf(config.isDev, sourcemaps.init()))
+        .pipe(scss())
+        .pipe(concat(config.output.cssName))
+        .pipe(autoprefixer())
+        .pipe(gulpIf(config.isDev, cleanCss()))
+        .pipe(gulpIf(config.isDev, sourcemaps.write()))
+        .pipe(gulp.dest(config.output.path))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: config.output.path
+        }
+    });
+    gulp.watch(config.paths.scss, ['scss']);
+    gulp.watch(config.paths.html).on('change', browserSync.reload());
+});
+
+gulp.task('default', ['scss', 'serve']);
